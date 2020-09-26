603cec43288a1af492fe366dfba4ace8344e204a
Tomer <55205856+TomerMir@users.noreply.github.com>
2020-09-26 15:02:48 +0300

    Update Question.java

--- src/main/java/com/eyal/bullshitwtf/Game/Question.java
+++ src/main/java/com/eyal/bullshitwtf/Game/Question.java
@@ -1,20 +1,29 @@
+import java.util.Arrays;
 package com.eyal.bullshitwtf.Game;
 
 public class Question {
     private String question;
+    private String answer;
+    private String[] bullshits;
 
-    public Question(String question) {
+    public Question(String question, String answer, String[] bullshits) {
         this.question = question;
+        this.answer = answer;
+        this.bullshits = bullshits;
     }
 
-    public Question() {
+    public String getAnswer(){
+        return this.answer;
     }
 
     public String getQuestion() {
         return question;
     }
 
-    public void setQuestion(String question) {
-        this.question = question;
+    public String[] getNumOfBullshits(int numOfBullshits){
+        if (numOfBullshits >= this.bullshits.length) {
+            throw new IndexOutOfBoundsException(numOfBullshits);
+        }
+        return Arrays.copyOfRange(Utils.shuffleArray(this.bullshits), 0, 2);
     }
 }
